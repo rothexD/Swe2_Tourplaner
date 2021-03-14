c8ff81d5c9c0a1224cc4f50f6a926b63a2cc6e5e
rothexD <56934789+rothexD@users.noreply.github.com>
2021-03-14 18:46:10 +0100

    Tour Model And LogEntry Model

---
+++ Swe2_tour_planer/Model/LogEntry.cs
@@ -0,0 +1,122 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Collections.ObjectModel;
+
+namespace Swe2_tour_planer.Model
+{
+    class LogEntry : INotifyPropertyChanged
+       {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string _date;
+        private int _duration;
+        private float _distance;
+        private int _rating;
+        private string _report;
+
+        private float _averageSpeed;
+        private int _energyUsed;
+        private string _wheater;
+        private string _traffic;
+        private int _nicenessOfLocals;
+
+        public LogEntry(string date,int duration,float distance,int rating,string report,float averageSpeed,int energyUsed,string wheater,string traffic,int nicenessOfLocals)
+        {
+
+        }
+        public string Date
+        {
+            get => this._date;
+            set
+            {
+                this._date = value;
+                this.OnPropertyChanged();
+            }
+        }
+        public int Duration
+        {
+            get => this._duration;
+            set
+            {
+                this._duration = value;
+                this.OnPropertyChanged();
+            }
+        }
+        public float Distance
+        {
+            get => this._distance;
+            set
+            {
+                this._distance = value;
+                this.OnPropertyChanged();
+            }
+        }
+        public int Rating
+        {
+            get => this._rating;
+            set
+            {
+                this._rating = value;
+                this.OnPropertyChanged();
+            }
+        }
+        public string Report
+        {
+            get => this._report;
+            set
+            {
+                this._report = value;
+                this.OnPropertyChanged();
+            }
+        }
+        public float AverageSpeed
+        {
+            get => this._averageSpeed;
+            set
+            {
+                this._averageSpeed = value;
+                this.OnPropertyChanged();
+            }
+        }
+        public int EnergyUsed
+        {
+            get => this._energyUsed;
+            set
+            {
+                this._energyUsed = value;
+                this.OnPropertyChanged();
+            }
+        }
+        public string Wheater
+        {
+            get => this._wheater;
+            set
+            {
+                this._wheater = value;
+                this.OnPropertyChanged();
+            }
+        }
+        public string Traffic
+        {
+            get => this._traffic;
+            set
+            {
+                this._traffic = value;
+                this.OnPropertyChanged();
+            }
+        }
+        public int NicenessOfLocals
+        {
+            get => this._nicenessOfLocals;
+            set
+            {
+                this._nicenessOfLocals = value;
+                this.OnPropertyChanged();
+            }
+        }
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ Swe2_tour_planer/Model/TourEntry.cs
@@ -0,0 +1,64 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Collections.ObjectModel;
+
+namespace Swe2_tour_planer.Model
+{
+    class TourEntry : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string _title;
+        private string _description;
+        private string _imgSource;
+        private ObservableCollection<LogEntry> _logEntries;
+        public TourEntry(string title,string description,string _imgSource,ObservableCollection<LogEntry> logEntries)
+        {
+            this.Title = title;
+            this.Description = description;
+            this.ImgSource = _imgSource;
+            this.LogEntries = logEntries;
+        }
+
+        public string Title
+        {
+            get => this._title;
+            set
+            {
+                this._title = value;
+                this.OnPropertyChanged();
+            }
+        }
+        public string Description
+        {
+            get => this._description;
+            set
+            {
+                this._description = value;
+                this.OnPropertyChanged();
+            }
+        }
+        public string ImgSource
+        {
+            get => this._imgSource;
+            set
+            {
+                this._imgSource = value;
+                this.OnPropertyChanged();
+            }
+        }
+        public ObservableCollection<LogEntry> LogEntries
+        {
+            get => this._logEntries;
+            set
+            {
+                this._logEntries = value;
+                this.OnPropertyChanged();
+            }
+        }
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
